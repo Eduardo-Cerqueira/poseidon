38258729faf86a3fe8e41592cee4d485ae067568
Eduardo-Cerqueira <eduardoccerqueira778@gmail.com>
2023-10-31 22:30:28 +0100

    :sparkles: Add forbidden route

--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, status
 
 from persistence.product_repository import fetch_all_products, fetch_product_by_id
 
@@ -10,14 +10,22 @@ def read_root():
     return {"message": "Hello World"}
 
 
-@app.get("/products")
+@app.get("/products", status_code=status.HTTP_200_OK)
 def read_products():
-    return {"status": 200, "products": fetch_all_products()}
+    return {"products": fetch_all_products()}
 
 
-@app.get("/products/{product_id}")
+@app.get("/products/{product_id}", status_code=status.HTTP_200_OK)
 def read_products_id(product_id: str):
-    return {"status": 200, "product": fetch_product_by_id(product_id)}
+    return {"product": fetch_product_by_id(product_id)}
+
+
+@app.get("/forbidden")
+def read_forbidden():
+    raise HTTPException(
+        status_code=status.HTTP_403_FORBIDDEN,
+        detail="You are not authorized",
+    )
 
 
 if __name__ == "__main__":
