1eece2eaa67bcbcb47b7213d04c0c563a1b368d7
Eduardo-Cerqueira <eduardoccerqueira778@gmail.com>
2023-10-31 23:13:02 +0100

    :sparkles: Add database product update using put

--- src/main.py
+++ src/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI, HTTPException, status
+from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
 
 from persistence.product_repository import (
     fetch_all_products,
     fetch_product_by_id,
     insert_product,
+    update_product
 )
 
 app = FastAPI()
@@ -12,6 +14,7 @@ app = FastAPI()
 
 # https://fastapi.tiangolo.com/tutorial/response-model
 class Product(BaseModel):
+    id: str | None = None
     code: str
     name: str
 
@@ -26,9 +29,14 @@ def read_products():
     return {"products": fetch_all_products()}
 
 
-@app.get("/products/{product_id}", status_code=status.HTTP_200_OK)
-def read_products_id(product_id: str):
+@app.get("/products/{product_id}", response_model=Product, status_code=status.HTTP_200_OK)
+def read_product(product_id: str):
     return {"product": fetch_product_by_id(product_id)}
+
+
+@app.put("/products/{product_id}", status_code=status.HTTP_204_NO_CONTENT)
+def update_product_by_id(product_id: str, product: Product):
+    return update_product(code=product.code, name=product.name, product_id=product_id)
 
 
 @app.get("/forbidden")
